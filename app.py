b4c4b347366a9f91f43d1c8bcc8c11cbd084ef1e
Asep Hendar <4sep.hendar@gmail.com>
2024-03-21 19:29:11 +0700

    Menambahkan migration

--- app.py
+++ app.py
@@ -1,21 +1,50 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/task_library'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-class books(db.Model):
+class Books(db.Model):
     id_book = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     author = db.Column(db.String(255), nullable=False)
     year = db.Column(db.Integer, nullable=False)
     total_pages = db.Column(db.Integer, nullable=False)
     category = db.Column(db.String(255), nullable=False)
+    
+    def __repr__(self) -> str:
+        return f'Book {self.id_book} - {self.title}'
+    
+    # def __repr__(self) -> str:
+    #     return super().__repr__()
+
+class Categories(db.Model):
+    id_category = db.Column(db.String(4), primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+
+class Authors(db.Model):
+    id_author = db.Column(db.String(4), primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    nationality = db.Column(db.String(255), nullable=False)
+    year_birth = db.Column(db.Integer, nullable=False)
 
 @app.route('/', methods=['GET'])
 def view_books():
-    data = books.query.all()
+    # data = Books.query.all()
+    # db.session.execute('SELECT * FROM books')
+    data = db.session.query(
+        Books.id_book,
+        Books.title,
+        Authors.name.label('author'),
+        Books.year,
+        Books.total_pages,
+        Categories.name.label('category')
+    ).join(Authors, Books.author == Authors.id_author).join(Categories, Books.category == Categories.id_category).order_by(Books.id_book.desc()).all()
+    
     books_list = []
     
     for el in data:
@@ -28,11 +57,12 @@ def view_books():
             'category': el.category
         })
         
-    return jsonify({'books': books_list})
+    return {'books': books_list}
 
 @app.route('/<int:id_book>', methods=['GET'])
 def view_book(id_book):
-    data = books.query.filter_by(id_book=id_book).first()
+    data = Books.query.filter_by(id_book=id_book).first()
+    print(data)
     book_data = {
         'id_book': data.id_book,
         'title': data.title,
@@ -41,12 +71,12 @@ def view_book(id_book):
         'total_pages': data.total_pages,
         'category': data.category
     }
-        
+    
     return jsonify({'book': book_data})
 
 @app.route('/', methods=['POST'])
 def add_book():
-    data = books(
+    data = Books(
         title = request.form['title'],
         author = request.form['author'],
         year = request.form['year'],
@@ -59,7 +89,7 @@ def add_book():
 
 @app.route('/<int:id_book>', methods=['PUT'])
 def update_book(id_book):
-    data = books.query.filter_by(id_book=id_book).first()
+    data = Books.query.filter_by(id_book=id_book).first()
     data.title = request.form['title']
     data.author = request.form['author']
     data.year = request.form['year']
@@ -70,7 +100,7 @@ def update_book(id_book):
 
 @app.route('/<int:id_book>', methods=['DELETE'])
 def delete_book(id_book):
-    data = books.query.filter_by(id_book=id_book).first()
+    data = Books.query.filter_by(id_book=id_book).first()
     db.session.delete(data)
     db.session.commit()
     return jsonify({'message': 'Book deleted'})
